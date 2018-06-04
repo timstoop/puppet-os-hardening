ad33da9097427c93079469ccd2d9db5e92f27baf
Tim Stoop <tim@kumina.nl>
2018-06-04 13:28:50 +0200

    WIP
    HardeningFramework-DCO-1.1-Signed-off-by: Tim Stoop <github@timstoop.nl> (github: timstoop)

---
+++ manifests/user_defaults.pp
@@ -0,0 +1,44 @@
+# === Copyright
+#
+# Copyright 2018, Kumina B.V., Tim Stoop
+# Licensed under the Apache License, Version 2.0 (the "License");
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+
+# == Class: os_hardening::user_defaults
+#
+# Configures user defaults for shells
+#
+class os_hardening::user_defaults (
+  Array   $extra_user_paths         = [],
+  String  $umask                    = '027',
+  String  $maildir                  = '/var/mail',
+  Boolean $usergroups               = true,
+  Integer $sys_uid_min              = 100,
+  Integer $sys_gid_min              = 100,
+  Integer $password_max_age         = 60,
+  Integer $password_min_age         = 7,
+  Integer $password_warn_age        = 7,
+  Integer $login_retries            = 5,
+  Integer $login_timeout            = 60,
+  String  $chfn_restrict            = '',
+  Boolean $allow_login_without_home = false,
+) {
+
+  # prepare all variables
+  $additional_user_paths = join($extra_user_paths, ':')
+
+  # convert bool to yes/no
+  $usergroups_yn = bool2str($usergroups, 'yes', 'no')
+
+  # set the file
+  file { '/etc/login.defs':
+      ensure  => file,
+      content => template('os_hardening/login.defs.erb'),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0444',
+  }
+
+}
+
