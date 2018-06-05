8c54c3ab123c7ff8299936358550aa646ee5952c
Tim Stoop <tim@kumina.nl>
2018-06-05 13:43:57 +0200

    Fix path permissions.
    HardeningFramework-DCO-1.1-Signed-off-by: Tim Stoop <github@timstoop.nl> (github: timstoop)

--- manifests/minimize_access.pp
+++ manifests/minimize_access.pp
@@ -95,5 +95,21 @@ class os_hardening::minimize_access (
     password => '*',
   }
 
+  $cron_directories = [
+    '/etc/cron.d',
+    '/etc/cron.monthly',
+    '/etc/cron.weekly',
+    '/etc/cron.daily',
+    '/etc/cron.hourly',
+  ]
+
+  # tighten restrictions on cron directory for CIS DIL Benchmark 5.1.3-7
+  file { $cron_directories:
+    ensure => directory,
+    mode   => 0700,
+    owner  => 'root',
+    group  => 'root',
+  }
+
 }
 
