3da1c517f1d634c3b3041960f9eda7eb5040a173
Tim Stoop <tim@kumina.nl>
2018-06-14 21:21:16 +0200

    Add to invocation as well.
    HardeningFramework-DCO-1.1-Signed-off-by: Tim Stoop <github@timstoop.nl> (github: timstoop)

--- manifests/init.pp
+++ manifests/init.pp
@@ -132,11 +132,13 @@ class os_hardening (
     allow_login_without_home => $allow_login_without_home,
   }
   class { 'os_hardening::minimize_access':
-    allow_change_user => $allow_change_user,
-    ignore_users      => $ignore_users,
-    shadowgroup       => $shadowgroup,
-    shadowmode        => $shadowmode,
-    recurselimit      => $recurselimit,
+    allow_change_user   => $allow_change_user,
+    ignore_users        => $ignore_users,
+    shadowgroup         => $shadowgroup,
+    shadowmode          => $shadowmode,
+    recurselimit        => $recurselimit,
+    strict_tcp_wrappers => $strict_tcp_wrappers,
+    allow_ssh_from      => $allow_ssh_from,
   }
   class { 'os_hardening::modules':
     disable_filesystems   => $disable_filesystems,
